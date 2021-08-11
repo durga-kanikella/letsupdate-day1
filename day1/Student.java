ebdf5c8166b3682b16b5eaa9f3aac64f50ab109f
Durga Kanikella <83055832+durga-kanikella@users.noreply.github.com>
2021-08-11 11:43:54 +0530

    day1 assignment submitted

---
+++ day1/Student.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Student{
+    public static void main (String[] args){
+        Scanner sc =new Scanner(System.in);
+       
+           int roll=sc.nextInt();
+           String name=sc.nextLine();
+           Float marks=sc.nextFloat();
+        
+       
+            System.out.println("Roll: "+ roll);
+            System.out.println("name: "+ name);
+            System.out.println("marks: "+ marks);
+        
+        
+    }
+}
